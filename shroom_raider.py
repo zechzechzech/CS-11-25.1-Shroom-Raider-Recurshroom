d3ab66acca8a12dab77e031ccd33fa18141120ec
zechzechzech <jt.sanpablo@up.edu.ph>
2025-10-18 03:55:05 +0800

    added moving via wasd using inputs, updated the map after ever movement, also added a way to move multiple times e.g. wwwdwas, also added tree collision although im not sure if there are bugs or not

--- shroom_raider.py
+++ shroom_raider.py
@@ -1,12 +1,15 @@
 from argparse import ArgumentParser
+import os
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-# returns the converted map as a LIST
-def convert_map(stage_map_rows: list) -> list:
+# returns the converted map
+def convert_map(stage_map: list) -> str:
     converted_map = []
 
     # goes through every row and character then appends to the converted_row list
-    for row in stage_map_rows:
+    for row in stage_map:
         converted_row = []
         for char in row:
             if char == "L":
@@ -29,8 +32,35 @@ def convert_map(stage_map_rows: list) -> list:
         # appends converted_row as a string to the converted_map list 
         converted_map.append("".join(converted_row))
 
-    # returns as a list btw
-    return converted_map
+    return "\n".join(converted_map)
+
+def get_player_position(stage_map: list):
+    for y, row in enumerate(stage_map):
+        for x, char in enumerate(row):
+            if char == "L":
+                return x, y
+
+
+# TODO: add things like push rock, pickup items and collision
+# updates player position
+def update_map(player_x, player_y, stage_map):
+    new_map = []
+
+    for y, row in enumerate(stage_map):
+        new_row = []
+
+        for x, char in enumerate(row):
+            if x == player_x and y == player_y:
+                new_row.append("L")
+            elif char == "L":
+                new_row.append(".")
+            else:
+                new_row.append(char)
+
+        new_map.append("".join(new_row))
+
+    return new_map
+
 
 
 def main():
@@ -40,10 +70,62 @@ def main():
     args = arg_parser.parse_args()
 
     # opens the txt file with the stage map
-    with open(args.stage_file, encoding="utf-8") as stage_map:
-        stage_map_list = stage_map.readlines()
-
-        print("\n".join(convert_map(stage_map_list)))
+    with open(args.stage_file, encoding="utf-8") as stage_file:
+        stage_map = stage_file.readlines()
 
 
+    # gets the player's original position
+    player_x, player_y = get_player_position(stage_map)
+
+
+    # clears the terminal
+    clear()
+    # converts ascii map to ui representation    
+    print(convert_map(update_map(player_x, player_y, stage_map)))
+
+    game_state = True
+    while game_state:
+        user_input = input("enter input: ").lower()
+
+        # stop if entered exit
+        if user_input == "exit":
+            game_state = False
+            break
+
+        # counts the amount of w's or whatever to move
+        for move in user_input:
+            target_x = player_x
+            target_y = player_y
+
+            # 0, 0 starts at top left so subtracting y moves upwards and vice versa
+            if move == "w":
+                target_y -= 1
+            
+            elif move == "s":
+                target_y += 1
+                
+            elif move == "a":
+                target_x -= 1
+                
+            elif move == "d":
+                target_x += 1
+
+            # if theres a tree, just dont update the map lmfao
+            if stage_map[target_y][target_x] == "T":
+                continue
+            else:
+                player_y = target_y
+                player_x = target_x
+
+        clear()
+        # converts ascii map to ui representation
+        print(convert_map(update_map(player_x, player_y, stage_map)))
+
+
+# P.S. IM SORRY TO WHOEVER IS READING AND CHANGING THIS CODE
+# NOTE: FEEL FREE TO CHANGE KAHIT SAAN SA CODE KO CUZ IK THAT IT LOOKS LIKE SHIT
+# LIKE MAKE IT PRETTIER AND MORE READABLE AND WORK BETTER OR WHATEVER
+# PWEDE RIN GAWA PA KAYO FUNCTIONS AND LIKE REPLACE THE CODE SA MAIN FUNCTION
+# CUZ LOWKEY LINAGAY KO SA MAIN FUNCTION YUNG KARAMIHAN
+# MAH BAD
 main()
